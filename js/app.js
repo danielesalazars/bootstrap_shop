a9c508db43ff1aee1cd0ede6c76ba2ee715a4091
Daniel Salazar <daniel.esalazars@gmail.com>
2017-09-06 12:40:53 -0500

    Validando formulario

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const $form = $('#suscribeteForm')
+const $formGroup = $form.find('.form-group')
+const $formControl = $formGroup.find('.form-control')
+
+$form.submit(function(event) {
+
+	event.preventDefault()
+	
+	const error = Math.random() > 0.5
+	var el;
+
+	$form.removeClass()
+	$formGroup.removeClass().addClass('form-group')
+	$formControl.removeClass().addClass('form-control')
+
+	$formGroup.find('.form-control-feedback').remove()
+
+	if (error) {
+		el = $('<div>',{ html: 'Ha ocurrido un error' })
+	}else{
+		el = $('<div>',{ html: 'Te enviaremos todas las novedades' })
+		el.addClass('text-success')
+	}
+	el.addClass('form-control-feedback invalid-feedback')
+	$form.addClass('was-validated')
+	$formGroup.append(el)
+
+})
