19b935a7d51e4fe50548f81780a6a1ecd9e1efa0
Daniel Salazar <daniel.esalazars@gmail.com>
2017-09-07 09:25:14 -0500

    Agregando icono a los filtros y haciendo que cambie

--- js/app.js
+++ js/app.js
@@ -38,3 +38,18 @@ function mostrarModal(){
 		localStorage.noMostrarModal = true
 	})
 }
+
+const $filtrosToggle = $('#filtrosToggle')
+$filtrosToggle.click(function(event) {
+
+	event.preventDefault()
+
+	const $i = $filtrosToggle.find('i.fa')
+	const isDown = $i.hasClass('fa-chevron-down')
+
+	if (isDown) {
+		$i.removeClass('fa-chevron-down').addClass('fa-chevron-up')
+	}else {
+		$i.removeClass('fa-chevron-up').addClass('fa-chevron-down')
+	}
+})
